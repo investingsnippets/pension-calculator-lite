d4e56009a33d4fde2dbdfda0bc3b35624fd586eb
investingsnippets <investingsnippets@gmail.com>
2023-02-19 17:30:00 +0100

    Better Parameters section visualisation

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,9 @@ market_rate_pre_retirement=market_rate_post_retirement=0
 
 with st.sidebar:
     st.sidebar.title("Parameters")
+    birth_date = st.date_input("Birth Date:", datetime(1980, 1, 1).date(), help="""The birht date is used to show nicer graphs and calculate retirement and terminal dates.
+> No personal data is saved. When you refresh or leave the page, the data will vanish. """)
+
     monthly_cost_now_net = st.number_input("Expected net monthly payment while in pension (in today's terms): ", min_value=0.0, format='%f', help=
         """As if today was your first day at retirement! Based on today's cost of living,
         how much money per month would you be happy living with?""")
@@ -48,29 +51,32 @@ with st.sidebar:
 > It will help with our calculations. In the next, more advanced, version of this calculator, we will be more specific about the amounts.
 """)
 
-    birth_date = st.date_input("Birth Date:", datetime(1980, 1, 1).date(), help="We use that to show nicer graphs")
+    with st.expander("Retirement Parameters"):
+        retirement_age = st.selectbox('Retirement Age:', tuple(i for i in range(55,75,1)), index=10, help="At what age do you plan to retire?")
+        life_expectancy = st.selectbox('Terminal Year:', tuple(i for i in range(55,115,1)), index=45, help="When do you estimate to stop withdrawing from your pension account?")
 
-    retirement_age = st.selectbox('Retirement Age:', tuple(i for i in range(55,75,1)), index=10, help="At what age do you plan to retire?")
+    with st.expander("Inflation and Taxes"):
+        inf_annual_post = st.number_input("Annualized Inflation Rate (%): ", min_value=0.0, format='%f', value=3.8, help="""Inflation in this version of the calculator is static and is used
+        to calculate the gross amount needed as a first payment at the retirement date. For example, in USA, during the observation period from 1960 to 2021, the average inflation rate was 3.8% per year""")
+        
+        growth_pre_retirement = st.number_input("Annual growth of your deposits (%): ", min_value=0.0, format='%f', value=3.8, help="""The percentage in growth of your deposits per year.
+            For example, one could increase as much as the inflation increases.""")
+        
+        growth_post_retirement = st.number_input("Annual growth of your withdrawals (%): ", min_value=0.0, format='%f', value=3.8, help="""The percentage in growth of your withdrawals per year.
+            For example, one could increase as much as the inflation increases.""")
+        
+        tax_rate = st.number_input("Post-Retirement Tax Rate (%): ", min_value=0.0, format='%f', help="""Usually, the gross amount you get paid by your pension account each month is taxed by the state!""")
 
-    market_rate_pre_retirement = st.number_input("Pre-Retirement Return (%): ", min_value=0.0, format='%f', value=5.26, help="""What is the annualized return on your pension porfolio?
-For example, if you have invested all your portfolio in an index fund, that could be the annualized return of the fund for the last 10 years.
-**Keep in mind that Historical returns are no guarantee of future returns!**""")
+    with st.expander("Market Returns Expectations"):
+        market_rate_pre_retirement = st.number_input("Pre-Retirement Return (%): ", min_value=0.0, format='%f', value=10.15, help="""What is the annualized return on your pension porfolio?
+            For example, the average annualized return ot the Standard & Poor's 500 Index (S&P 500) from 1957 through Dec. 31, 2022, is 10.15%.
+            **Keep in mind that historical returns are no guarantee of future returns!**""")
 
-    growth_pre_retirement = st.number_input("Annual growth of your deposits (%): ", min_value=0.0, format='%f', value=3.35747, help="""The percentage in growth of your deposits per year.
-For example, one could increase as much as the inflation increases.""")
+        market_rate_post_retirement = st.number_input("Post-Retirement Return (%): ", min_value=0.0, format='%f', value=10.15, help="""What is the annualized return on your pension porfolio?
+            For example, the average annualized return ot the Standard & Poor's 500 Index (S&P 500) from 1957 through Dec. 31, 2022, is 10.15%.
+            **Keep in mind that historical returns are no guarantee of future returns!**""")
 
-    life_expectancy = st.selectbox('Terminal Year:', tuple(i for i in range(55,115,1)), index=45, help="When do you estimate to stop withdrawing from your pension account?")
-
-    tax_rate = st.number_input("Post-Retirement Tax Rate (%): ", min_value=0.0, format='%f', help="""Usually the gross amount you get paid by your pension account each month is taxed!""")
-
-    market_rate_post_retirement = st.number_input("Post-Retirement Return (%): ", min_value=0.0, format='%f', value=5.26, help="""What is the annualized return on your pension porfolio?
-For example, if you have invested all your portfolio in an index fund, that could be the annualized return of the fund for the last 10 years.
-**Keep in mind that Historical returns are no guarantee of future returns!**""")
-
-    growth_post_retirement = st.number_input("Annual growth of your withdrawals (%): ", min_value=0.0, format='%f', value=3.35747, help="""The percentage in growth of your withdrawals per year.
-For example, one could increase as much as the inflation increases.""")
-
-    inf_annual_post = st.number_input("Annualized Inflation Rate (%): ", min_value=0.0, format='%f', value=3.35747)
+        
 
 if market_rate_pre_retirement == growth_pre_retirement:
     st.error("""Error! `Pre-Retirement Return` must be different than `Annual growth of your deposits`""")
@@ -171,12 +177,12 @@ st.plotly_chart(fig, use_container_width=True, theme="streamlit")
 st.header("**Current Financial Situation**")
 
 st.markdown(f"""
-You were born {birth_date} and you celebrated your most recent birthday on {most_resent_birthday}.
+You were born {birth_date} and you celebrated your most recent birthday on {most_resent_birthday} :fireworks:.
 You plan to retire on {retirement_date} and continue withdrawing from your pension account sometime in year {terminal_date.year}!
 
 You have {years_to_retirement} years until you retire, and from that point, you have {count_down_years} years until the end of your pension plan.
 
-As of {most_resent_birthday}, you have saved a total of {initial_amount_for_pension:.2f}.
+As of {most_resent_birthday}, you have saved a total of {initial_amount_for_pension:,}.
 
 Let's move on to the next section and visualise what will happen when you are in retirement.
 """)
@@ -203,32 +209,32 @@ For example, with an inflation rate of 10%, the same product you bought for \$1 
 
 The same applies to pensions. With an annual inflation rate of 5%, \$1 today will be worth \${1*(1+0.05)**years_to_retirement:.2f} when you retire :dizzy_face:!
 
-With today's cost of living standards, you would feel comfortable with a monthly pension payment of {monthly_cost_now_net:.2f}.
-That is an annual cost, in today's terms, of {annual_cost_now_net:.2f}.
+With today's cost of living standards, you would feel comfortable with a monthly pension payment of {monthly_cost_now_net:,.2f}.
+That is an annual cost, in today's terms, of {annual_cost_now_net:,.2f}.
 
-With an inflation rate of {inf_annual_post*100.0:.2f}%, the net amount you will need as a first pension salary is {monthly_needed_at_retirement_net:.2f}.
-That is an annual amount of {annual_needed_at_retirement_net:.2f}. Since the retirement tax rate is {tax_rate*100.0:.2f}%, the gross amount you will need
-to pay yourself the moment you start your retirement ({retirement_date}) should be {annual_needed_at_retirement_gross:.2f}.
+With an inflation rate of {inf_annual_post*100.0:.2f}%, the net amount you will need as a first pension salary is {monthly_needed_at_retirement_net:,.2f}.
+That is a gross annual amount of {annual_needed_at_retirement_net:,.2f}. Since the retirement tax rate is {tax_rate*100.0:.2f}%, the gross amount you will need
+to pay yourself the moment you start your retirement ({retirement_date}) should be {annual_needed_at_retirement_gross:,.2f}.
 
-As you can see in the `CashFlows` section of the image below, an initial annual amount of {annual_needed_at_retirement_gross:.2f}
-needs to be paid to you at {retirement_date}. Of cource, you will split it down to equal monthly payments throughout the year.
-However, on the `{retirement_date.month}-{retirement_date.day}` of each consecutive year, your total account balance should be able to 
+As you can see in the `CashFlows` section of the image below, an initial annual amount of {annual_needed_at_retirement_gross:,.2f}
+needs to be paid to you on {retirement_date} at the age of {retirement_age}. Of cource, you will split it down to equal monthly payments throughout the year.
+However, on the `{retirement_date.day}` day of the `{retirement_date.month}` month of each consecutive year, your total account balance should be able to 
 allow you to withdraw the annual amount you need and continue growing with the remaining balance.
 
-Now, due to inflation, to be able to keep the same quality of leaving while in pension, you will need to grow
+Now, due to inflation, to be able to keep the same standard of leaving while in pension, you will need to grow
 the annual amount you pay yourself by a growth rate. This rate is set to {growth_post_retirement*100.0:.2f}%.
 That means that at the beginning of the second year after retirement ({retirement_date+relativedelta(years=1)}) you will
 need to pay yourself {annual_needed_at_retirement_gross * (1+growth_post_retirement)**1:.2f},
-the third year ({retirement_date+relativedelta(years=2)}) {annual_needed_at_retirement_gross * (1+growth_post_retirement)**2:.2f} and so on.
+the third year ({retirement_date+relativedelta(years=2)}) {annual_needed_at_retirement_gross * (1+growth_post_retirement)**2:,.2f} and so on.
 At the start of the terminal year ({terminal_date-relativedelta(years=1)}) you will need
-an whopping annual amount of {pension_balance['InflatedOutflow'][-1]:.2f} :cold_sweat:.
+an whopping annual amount of {pension_balance['InflatedOutflow'][-1]:,.2f} :cold_sweat:.
 
 All these annual payments will need to be somehow financed, of cource! The second part of the graph below shows
 the optimal balance of your pension account at the beginning of each year.
 The amount shown per year is _after_ you have withdrawn your annual payment.
 
-On {retirement_date} you should have a total of {pension_balance.Balance[0]:.2f} :open_mouth:.
-The day before ({retirement_date-relativedelta(days=1)}) the balance should be {pension_balance.Balance[0] + pension_balance.InflatedOutflow[0]:.2f}.
+On {retirement_date} you should have a total of {pension_balance.Balance[0]:,.2f} :open_mouth:.
+The day before ({retirement_date-relativedelta(days=1)}) the balance should be {pension_balance.Balance[0] + pension_balance.InflatedOutflow[0]:,.2f}.
 
 The last payment date is on {terminal_date-relativedelta(years=1)}, and you should see a 0 balance in your account (since you withdrew the last annual payment).
 """)
@@ -254,22 +260,22 @@ st.header("**The path towards retirement**")
 # assert pension_balance.Balance[0].round(2) + pension_balance.InflatedOutflow[0].round(2) == pre_pension_balance.Balance[-1].round(2) # The balance we have in the account when we first get into pension is after we have taken out the first pension payment 
 
 st.markdown(f"""
-Well, we know that just the day before {retirement_date}, you will need to have a total of {pension_balance.Balance[0] + pension_balance.InflatedOutflow[0]:.2f} in your
-pension account. On {retirement_date} you will withdraw the first pension annual amount of {pension_balance.InflatedOutflow[0]:.2f}, leaving you with {pension_balance.Balance[0]:.2f}).
+Well, we know that just the day before {retirement_date}, you will need to have a total of {pension_balance.Balance[0] + pension_balance.InflatedOutflow[0]:,.2f} in your
+pension account. On {retirement_date} you will withdraw the first pension annual amount of {pension_balance.InflatedOutflow[0]:,.2f}, leaving you with {pension_balance.Balance[0]:,.2f}).
 
 To achieve this plan, you need to invest. That is usually done by placing the money in an investment account.
 You estimate an annual rate of return of {market_rate_pre_retirement*100.0:.2f}% from your investments.
 You also think that you should be increasing your annual deposits to your pension account by {growth_pre_retirement*100:.2f}% (to account for inflation, at least).
 
-According to the information above, you would need a total of {pv_pension_most_resent_birthday:.2f} in your account as of {most_resent_birthday}. If you had that,
+According to the information above, you would need a total of {pv_pension_most_resent_birthday:,.2f} in your account as of {most_resent_birthday}. If you had that,
 and the market was in your favor for the years to come (at an annual {market_rate_pre_retirement*100.0:.2f}%) you wouldn't need to make any more deposits to your
 account again and could retire properly on {retirement_date}.
 
-However, your current balance is {initial_amount_for_pension:.2f}, and in order to reach your target,
+However, your current balance is {initial_amount_for_pension:,.2f}, and in order to reach your target,
 you will need to be depositing money to your account.
 
-So far, you have to be depositing **{initial_annual_deposit_amount/12:.2f} per month** ({initial_annual_deposit_amount:.2f} annual) to your
-account, and each time you celebrate your next birthday, you should be increasing that amount by {growth_pre_retirement*100:.2f}% (what a costly celebration :gift: :stuck_out_tongue:).
+So far, you have to be depositing **{initial_annual_deposit_amount/12:,.2f} per month** ({initial_annual_deposit_amount:,.2f} annual) to your
+account, and each time you celebrate your next birthday, you should be increasing that amount by {growth_pre_retirement*100:.2f}%.
 """)
 
 time_range_from_now_to_retirement = pd.to_datetime(pd.Series([f'{i}-{retirement_date.month}-{retirement_date.day}' for i in range(most_resent_birthday.year, retirement_date.year+1,1)]),format='%Y-%m-%d')
